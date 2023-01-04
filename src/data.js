491abfaee99ea77784972d86e0285e1088bc5652
Yauheni Znak <120500388+yauheniznak@users.noreply.github.com>
2023-01-04 12:31:50 -0500

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,40 @@
+export const data =[
+    {
+        id:1,
+        photo:"https://media.loropiana.com/HYBRIS/FAM/FAM7284/F02C8C61-CD96-4CD4-B378-8C22D99746F2/FAM7284_J1CH_MEDIUM.jpg",
+        name:"Snow Wander Trousers",
+        country:"Made in Italy",
+        description:"Trousers made from baby cashmere yarn. When worn with a sweatshirt, they create a soft, warm tracksuit that is perfect on the coldest days of the year. A Loro Piana Mountain patch and sporty finishings complete the look",
+        fabric:"Baby Cashmere",
+        price:"$2,500.00",
+    },
+    {
+        id:2,
+        photo:"https://media.loropiana.com/HYBRIS/FAM/FAM6850/7DD27EF2-ACF7-4B1A-AFCE-0324D1B84DF4/FAM6850_F3Y7_MEDIUM.jpg",
+        name:"Icer Snow Wander Jacket",
+        country:"Made in Italy",
+        description:"Padded cashmere jacket featuring a Scottish design. Thanks to a special Storm System® treatment, it is waterproof and wind-resistant. Inspired by the historic Icer style, its wide, comfortable volumes and iconic details provide a versatile, practical allure.",
+        fabric:"Cashmere - Storm System®",
+        price:"$7,250.00",
+
+    },
+    {
+        id:3,
+        photo:"https://media.loropiana.com/HYBRIS/FAM/FAM6140/7DF85FF3-3B18-4A91-976F-9AD131A2EE61/FAM6140_F3ZM_MEDIUM.jpg",
+        name:"Bernina Bomber Jacket",
+        country:"Made in Italy",
+        description:"Fluffy, warm bomber jacket made from distinctively soft Coarsehair® cashmere yarn. Its luxuriously crafted jacquard motif provides a sporty allure.",
+        fabric:"Cashmere",
+        price:"$3,550.00", 
+    },
+    {
+        id:4,
+        photo:"https://media.loropiana.com/HYBRIS/FAM/FAM5928/EBA757B4-F53B-4FC9-A241-E8021A696E64/FAM5928_F3X0_MEDIUM.jpg",
+        name:"Colorama Socks",
+        country:"Made in Italy",
+        description:"Plain-knit socks made from extremely lightweight, soft Wish® wool. Their multi-colored pattern provides a sophisticated allure, making them perfect in casual outfits.",
+        fabric:"Virgin Wool",
+        price:"$125.00", 
+    }
+    
+]
