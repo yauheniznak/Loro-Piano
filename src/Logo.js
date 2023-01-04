bf5f4eaec7fa14e668ac2ebed8dc41a0e9ccd91e
Yauheni Znak <120500388+yauheniznak@users.noreply.github.com>
2023-01-04 12:33:16 -0500

    Create Logo.js

---
+++ src/Logo.js
@@ -0,0 +1,15 @@
+import image from './loro.jpeg';
+
+function Logo(){
+    return(
+     
+
+        
+        <div>
+             <img src={image} alt="logo" width="200px" height="100px"/>
+        </div>
+        
+    )
+}
+
+export default Logo
